c7619aeb5108aec9133b33a9c8b424a148c7ff5c
JOY <joybarua0058@gmail.com>
2023-07-31 18:25:01 +0600

    custom error page done

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,15 @@
+import { useRouter } from "next/router";
+
 const NotFoundPage = () => {
+  const router = useRouter();
+
+  setTimeout(() => {
+    router.push("/");
+  }, 5000);
+
   return (
-    <div className="flex justify-center">
-      <img src="/images/404-error.jpg" alt="" width={"70%"} />
+    <div className="flex justify-center items-center">
+      <img src="/images/404-error.jpg" alt="" width={"65%"} />
     </div>
   );
 };
