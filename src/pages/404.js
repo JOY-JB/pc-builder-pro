9f28ba6a5b59d2de2b825ac019e9805911244390
JOY-JB <joybarua0058@gmail.com>
2023-07-31 17:55:16 +0600

    change device

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+  return (
+    <div className="flex justify-center">
+      <img src="/images/404-error.jpg" alt="" width={"70%"} />
+    </div>
+  );
+};
+
+export default NotFoundPage;
---
+++ src/pages/pc-builder.js
@@ -0,0 +1,5 @@
+const PcBuilderPage = () => {
+  return <div></div>;
+};
+
+export default PcBuilderPage;
